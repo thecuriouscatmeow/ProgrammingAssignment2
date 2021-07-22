6cc263a48afb99b7eb7002326d7e03e7d37e25d2
thecuriouscatmeow <thecuriouscat_meow@icloud.com>
2021-07-22 18:21:46 +0530

    catcheMatrix updated to include functions for solving inverse of matrix by catcheing solution

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,36 @@
 ## Put comments here that give an overall description of what your
 ## functions do
 
-## Write a short comment describing this function
+## A pair of functions that cache the inverse of a matrix.
+## This function creates a special "matrix" object that can cache its inverse.
 
 makeCacheMatrix <- function(x = matrix()) {
+  m <- NULL
+  set <- function(y) {
+    x <<- y
+    m <<- NULL
+  }
+  get <- function() x
+  setInverse <- function(solveMatrix) m <<- solveMatrix
+  getInverse <- function() m
+  list(set = set, get = get,
+       setInverse = setInverse,
+       getInverse = getInverse)
 
 }
 
 
-## Write a short comment describing this function
 
-cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
-}
+## This function computes the inverse of the special "matrix" returned by makeCacheMatrix above.
+  cacheSolve <- function(x, ...) {
+    ## Returns a matrix that is the inverse of 'x'
+    m <- x$getInverse()
+    if(!is.null(m)) {
+      message("getting cached data")
+      return(m)
+    }
+    data <- x$get()
+    m <- solve(data, ...)
+    x$setInverse(m)
+    m
+  }
